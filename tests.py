86548640921a85df00c4d8366ada1e4d1ebe2f79
Jim <tianwai263@gmail.com>
2022-01-08 19:47:57 +0800

    add extra info

--- tests.py
+++ tests.py
@@ -7,6 +7,9 @@ import string
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "./trdg")))
 # comment for test!
+
+# 1. commit
+# 2. push
 try:
     os.mkdir("tests/out")
 except:
